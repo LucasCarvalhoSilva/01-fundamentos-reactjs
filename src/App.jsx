6c614f04f22f149c7c6235784f71f716751fc460
Lucas Carvalho Silva <lucascarvalhosilva2901@gmail.com>
2023-02-07 22:00:44 -0300

    adicionei propriedades ao component Post

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,15 @@ import {Post} from "./Post"
 export function App() {
   return (
     <div>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-    </div>
+      <Post 
+        author="Lucas Carvalho Silva" 
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis minima aliquid perspiciatis necessitatibus, quam ex esse laborum, repellendus       molestiae vel illo nesciunt nisi, asperiores assumenda doloremque ipsum maxime. Rem, voluptatum?"
+      />
+      <Post 
+        author="Gabriel Buzzi" 
+        content="Um novo post muito legal"
+      />
+   </div>
   )
 }
+
