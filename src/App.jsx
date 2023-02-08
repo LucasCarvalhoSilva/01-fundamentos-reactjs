d94c3756a408ce8492b58a92beb54a6afab5f91e
Lucas Carvalho Silva <lucascarvalhosilva2901@gmail.com>
2023-02-08 15:17:13 -0300

    criei o primeiro componente e um css module para ele

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Header } from "./components/Header"
 import {Post} from "./Post"
 
 import './styles.css'
@@ -5,6 +6,8 @@ import './styles.css'
 export function App() {
   return (
     <div>
+      
+      <Header />
       <Post 
         author="Lucas Carvalho Silva" 
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis minima aliquid perspiciatis necessitatibus, quam ex esse laborum, repellendus       molestiae vel illo nesciunt nisi, asperiores assumenda doloremque ipsum maxime. Rem, voluptatum?"
