4414c21e98cb5394e8c6a0ad343174acce1bbec5
Lucas Carvalho Silva <lucascarvalhosilva2901@gmail.com>
2023-02-09 11:02:25 -0300

    criei o componente comentario

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,5 @@
+import { Comment } from './Comment'
+
 import styles from './Post.module.css'
 
 export function Post() {
@@ -19,6 +21,23 @@ export function Post() {
                 <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
                 <p>👉 <a href=''>jane.design/doctorcare</a></p>
                 <p> <a href=''>#novoprojeto</a>{' '} <a href=''>#nlw</a>{' '} <a href=''>#rocketseat</a>{' '}</p>
+            </div>
+
+            <form className={styles.commentForm}>
+                <strong>Deixe seu feedback</strong>
+                <textarea 
+                    placeholder='deixe um comentário'
+                />
+                
+                <footer>
+                    <button type='submit'>Publicar</button>
+                </footer>
+            </form>
+
+            <div className={styles.commentList}>
+                <Comment />
+                <Comment />
+                <Comment />
             </div>
         </article>
     )
