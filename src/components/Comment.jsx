a9a4f8a4e7fe4d2befac2bc33df2d3f1845ed74a
Lucas Carvalho Silva <lucascarvalhosilva2901@gmail.com>
2023-02-09 17:29:22 -0300

    adicionei as propriedades restantes

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,7 +2,7 @@ import { ThumbsUp, Trash } from 'phosphor-react'
 import { Avatar } from './Avatar'
 import styles from './Comment.module.css'
 
-export function Comment() {
+export function Comment({content}) {
     return (
         <div className={styles.comment}>
             <Avatar hasBorder={false} src="https://github.com/LucasCarvalhoSilva.png"  />
@@ -15,7 +15,7 @@ export function Comment() {
                         </div>
                         <button title='Deletar comentário'><Trash size={24}/></button>
                     </header>
-                    <p>Muito bom Devon, parabéns</p>
+                    <p>{content}</p>
                 </div>
                 <footer>
                     <button>
